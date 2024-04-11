1dfdcc4085e60ebb6a718c05d7a5330095c0b890
Sukmadi <nhatthanhdinh482@gmail.com>
2024-04-11 14:47:28 +0700

    Add project files.

---
+++ ProductWebMVC/wwwroot/css/product.css
@@ -0,0 +1,38 @@
+﻿.home {
+    font-family: 'Arial', sans-serif;
+    background-color: #222; /* Dark background */
+    color: #f0f0f0; /* Off-white text */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+}
+
+#container {
+    background-color: #383838; /* Slightly lighter background */
+    padding: 30px;
+    border-radius: 8px;
+    box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);
+}
+
+#product-list div {
+    border-bottom: 1px solid #444; /* Subtle divider */
+    padding: 15px 0;
+    margin-bottom: 10px;
+}
+
+button {
+    background-color: #663399; /* Purple accent */
+    color: white;
+    border: none;
+    padding: 12px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s; /* Smooth hover effect */
+}
+
+button:hover {
+    background-color: #804dbe; /* Slightly brighter purple */
+}
+
